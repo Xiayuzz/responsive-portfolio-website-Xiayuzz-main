332dea1ef263b5ccd90400c3287913adaaf215cd
Xiayuzz <119193834+Xiayuzz@users.noreply.github.com>
2023-04-12 15:31:46 +0800

    Update main.js
    修复了菜单栏的显示问题

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,9 +132,9 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
